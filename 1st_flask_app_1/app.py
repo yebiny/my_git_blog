289340578bb751c61ab9681ced49b29ea964a061
Nibey <50440208+yebiny@users.noreply.github.com>
2022-08-09 10:16:29 +0900

    Delete 1st_flask_app_1 directory

--- 1st_flask_app_1/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__) # 새로운 플라스크 인스턴스를 초기화한다.
-@app.route('/) # 라우트 데코레이터, 특정 URL이 index 함수를 실행하도록 지정
-def index():
-  return render_template('first_app.html')
-if __name__ =='__main__':
-  app.run()
